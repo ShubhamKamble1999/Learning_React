b09bdfd2347b8d1b2e4c3ed223628cd706ff2e3e
Shubham Kamble <shubhamkamblem9881@gmail.com>
2023-09-20 14:53:25 +0530

    Done with currency

---
+++ 04_currency_converter/README.md
@@ -0,0 +1,6 @@
+# CURRENCY CONVERTER
+* About project
+    * The project takes the amount as input from user.
+    * A custom hook calls the currency api to fetch all data.
+
+![Alt text](image.png)
---
+++ 04_currency_converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+
+function useCurrencyInfo(currency) {
+
+    const [data, setData] = useState({})
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log("DATA 1 :: ",res[currency]);
+                return setData(res[currency])
+            })
+    },[currency])
+
+    return data;
+
+}
+
+export default useCurrencyInfo // idea is to return whole method and then we can have access to data
